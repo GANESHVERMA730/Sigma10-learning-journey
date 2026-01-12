a3e7be2daba01ab6baaf4075cd28c03ba1acbb97
Ganesh Verma <bibekverma21@gmail.com>
2026-01-12 22:46:07 +0545

    added part 9 learnings

---
+++ Java Script/js part 9/app.js
@@ -0,0 +1,7 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for (let i = 0; i < smallImages.length; i++) {
+//   smallImages[i].src = "assets/spiderman_img.png";
+//   console.dir(`value of image no. ${i} is changed.`);
+// }
+// console.log(document.getElementsByClassName("hbsj"));
