2e5a0f1866063a503c14671146361fb6aef06465
Ganesh Verma <bibekverma21@gmail.com>
2026-02-02 22:52:54 +0530

    more code added

--- Java Script/js part 12/script.js
+++ Java Script/js part 12/script.js
@@ -31,7 +31,58 @@ function getNum() {
 }
 
 async function demo() {
-  getNum();
-  getNum();
+  await getNum();
+  await getNum();
+  await getNum();
+  await getNum();
+  await getNum();
+  await getNum();
   getNum();
 }
+
+h1 = document.querySelector("h1");
+
+function changeColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 5) + 1;
+      if (num > 3) {
+        reject("promise rejected");
+      }
+
+      h1.style.color = color;
+      console.log(`color changed to ${color}!`);
+      resolve("color changed!")
+    }, delay);
+  });
+}
+
+async function demo() {
+  try{
+  await changeColor("red", 1000);
+  await changeColor("yellow", 1000);
+  await changeColor("green", 1000);
+  await changeColor("purple", 1000);
+  await changeColor("blue", 1000);
+  }catch(err){
+    console.log("error caught");
+    console.log(err);
+  }
+
+
+  let a = 7
+  console.log(a);
+  console.log("new Number = ", a + 3);
+}
+
+// APIs concept 
+let jsonRes = '{"fact":"Cats see six times better in the dark and at night than humans.","length":63}'
+
+// console.log(jsonRes.fact);
+let validRes = JSON.parse(jsonRes);
+console.log(validRes.fact);
+
+let student = {
+  name: "bibek",
+  marks: 67,
+};
