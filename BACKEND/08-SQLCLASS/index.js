b4be9a63a652a9eb9cd1b85969ec7abdd0874bc4
Ganesh Verma <bibekverma21@gmail.com>
2026-03-09 18:35:04 +0530

    adding more code

--- BACKEND/08-SQLCLASS/index.js
+++ BACKEND/08-SQLCLASS/index.js
@@ -1,5 +1,12 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
 
 // Create the connection to database
 const connection = mysql.createConnection({
@@ -27,23 +34,58 @@ let getRandomUser = () => {
   ];
 }
 
-let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
 
-let data = [];
-for(let i=1; i<=100; i++){
-  data.push(getRandomUser());  // data of 100 fake users:
-}
+// let data = [];
+// for(let i=1; i<=100; i++){
+//   data.push(getRandomUser());  // data of 100 fake users:
+// }
 
-try {
-  connection.query(q, [data], (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    // console.log(result.length);
-  });
-} catch (err) {
-  console.log(err);
-}
+// try {
+//   connection.query(q, [data], (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     // console.log(result.length);
+//   });
+// } catch (err) {
+//   console.log(err);
+// }
 
-connection.end();
+// connection.end();
 
 
+// home rout e
+app.get("/", (req, res) => {
+  let q = `SELECT count(*) FROM user`;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let count = result[0]["count(*)"];
+      res.render("home.ejs", { count });
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("some errors in DB");
+  }
+});
+
+// grt/show users route
+app.get("/user", (req, res) => {
+  let q = `SELECT * FROM user`;
+
+  try {
+    connection.query(q, (err, users) => {
+      if (err) throw err;
+      // res.send(result);
+      res.render("showusers.ejs", {users});
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("some errors in DB");
+  }
+});
+
+
+app.listen("8080", () => {
+  console.log("server is listening to port 8080");
+});
