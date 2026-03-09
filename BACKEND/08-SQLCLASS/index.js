3c040182eb80942eccb3e5ce3b519fa996460417
Ganesh Verma <bibekverma21@gmail.com>
2026-03-09 16:24:38 +0530

    adding some sql learnings

--- BACKEND/08-SQLCLASS/index.js
+++ BACKEND/08-SQLCLASS/index.js
@@ -12,14 +12,30 @@ const connection = mysql.createConnection({
 // let q = "SHOW TABLES"; // we can also make a variable like this fir query and use it.
 
 // Inserrting new data.
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let users = [
+//   ['7342d', 'guud_123b', 'newemailb@gmail.com', 'lodu@123b'],
+//   ['7323e', 'gure_123c', 'newemailc@gmail.com', 'lond@123c'],
+// ];
+
+let getRandomUser = () => {
+  return [
+    faker.string.uuid(),
+    faker.internet.username(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+}
+
 let q = "INSERT INTO user (id, username, email, password) VALUES ?";
-let users = [
-  ["7323b", "guru_123b", "choducid@femail.comb", "lodu@123b"],
-  ["7323c", "guru_123c", "choducid@femail.comc", "lodu@123c"],
-];
+
+let data = [];
+for(let i=1; i<=100; i++){
+  data.push(getRandomUser());  // data of 100 fake users:
+}
 
 try {
-  connection.query(q, [users], (err, result) => {
+  connection.query(q, [data], (err, result) => {
     if (err) throw err;
     console.log(result);
     // console.log(result.length);
@@ -30,11 +46,4 @@ try {
 
 connection.end();
 
-let getRandomUser = () => {
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-  };
-}
+
