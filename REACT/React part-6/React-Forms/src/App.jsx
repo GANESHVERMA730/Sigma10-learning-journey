8e1e7ad007e8702299d40c699487e88c5d0827f3
Ganesh Verma <bibekverma21@gmail.com>
2026-06-12 19:19:06 +0545

    finally completed the React Learnings. next I will move to build a React based Weather App

--- REACT/React part-6/React-Forms/src/App.jsx
+++ REACT/React part-6/React-Forms/src/App.jsx
@@ -3,13 +3,15 @@ import './App.css';
 import Form from './Forms';
 import CommentsForm from './CommentsForm';
 import Comment from './Comment';
+import Counter from './Counter';
+import Joker from './Joker';
 
 function App() {
   return (
     <>
       {/* <CommentsForm /> */}
       <br /> <br />
-      <Comment />
+      <Joker />
     </>
   );
 }
