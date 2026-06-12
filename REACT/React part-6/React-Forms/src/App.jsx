830275fa8a37b44dad7d641dae782ac21fa47439
Ganesh Verma <bibekverma21@gmail.com>
2026-06-11 22:03:29 -0600

    completed show comments Learning

--- REACT/React part-6/React-Forms/src/App.jsx
+++ REACT/React part-6/React-Forms/src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Form from './Forms';
+import CommentsForm from './CommentsForm';
+import Comment from './Comment';
 
 function App() {
   return (
     <>
-      <Form />
+      {/* <CommentsForm /> */}
+      <br /> <br />
+      <Comment />
     </>
   );
 }
---
+++ REACT/React part-6/React-Forms/src/Comment.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./comment.css";
+import CommentsForm from "./CommentsForm";
+
+export default function Comment() {
+  let [comments, setComments] = useState([
+    {
+      username: "@sk",
+      remarks: "great job",
+      rating: 3,
+    },
+  ]);
+
+  let addNewComment = (comment) => {
+    setComments((currComments) => [...currComments, comment]);
+  };
+
+  return (
+    <>
+      <div>
+        <h2>All comments</h2>
+
+        {comments.map((comment, idx) => (
+          <div className="comment" key={idx}>
+            <span>{comment.remarks}</span>
+            &nbsp;
+            <span>(rating = {comment.rating})</span>
+            <p>- {comment.username}</p>
+          </div>
+        ))}
+      </div>
+
+      <hr></hr>
+
+      <CommentsForm addNewComment={addNewComment} />
+    </>
+  );
+}
