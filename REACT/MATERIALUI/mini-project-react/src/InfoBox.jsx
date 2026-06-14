a89b9bcfca1eb702cfd3b86284d5c901da068bf5
Ganesh Verma <bibekverma21@gmail.com>
2026-06-14 18:44:38 +0545

    added more features in the weather app and done with this Minor project

--- REACT/MATERIALUI/mini-project-react/src/InfoBox.jsx
+++ REACT/MATERIALUI/mini-project-react/src/InfoBox.jsx
@@ -1,35 +1,65 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 import "./InfoBox.css";
 
-export default function InfoBox({info}) {
-  const INIT_URL = "https://images.unsplash.com/photo-1601297183305-6df142704ea2?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-  
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import SunnyIcon from "@mui/icons-material/Sunny";
+
+export default function InfoBox({ info }) {
+  const INIT_URL =
+    "https://images.unsplash.com/photo-1601297183305-6df142704ea2?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const HOT_URL =
+    "https://images.unsplash.com/uploads/14121010130570e22bcdf/e1730efe?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+  const COLD_URL =
+    "https://images.unsplash.com/photo-1668531387310-9c3c2f272d52?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+  const RAIN_URL =
+    "https://plus.unsplash.com/premium_photo-1664303017917-71ebeb42343d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cmFpbiUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+
   return (
-    <div className='InfoBox'>
-      <div className='cardContainer'>
-      <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={INIT_URL}
-        title="clear sky"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {info.city}
-        </Typography>
-        <Typography variant="body2" color='text.secondary' component={"span"}>
-          <p>Temperature = {info.temp}&deg;C</p>
-          <p>Humidity = {info.humidity}</p>
-          <p>Min Temp = {info.temp_min}</p>
-          <p>Max Temp = {info.temp_max}</p>
-          <p>The weather can be described as <i>{info.weather}</i> and feels like = {info.feels_like}&deg;C</p>
-        </Typography>
-      </CardContent>
-    </Card>
-    </div>
+    <div className="InfoBox">
+      <div className="cardContainer">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={
+              info.humidity > 80
+                ? RAIN_URL
+                : info.temp > 15
+                  ? HOT_URL
+                  : COLD_URL
+            }
+            title="clear sky"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {info.city} {
+              info.humidity > 80
+                ? <ThunderstormIcon />
+                : info.temp > 15
+                  ? <SunnyIcon />
+                  : <AcUnitIcon />
+                  }
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              component={"span"}
+            >
+              <p>Temperature = {info.temp}&deg;C</p>
+              <p>Humidity = {info.humidity}</p>
+              <p>Min Temp = {info.temp_min}</p>
+              <p>Max Temp = {info.temp_max}</p>
+              <p>
+                The weather can be described as <i>{info.weather}</i> and feels
+                like = {info.feels_like}&deg;C
+              </p>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
